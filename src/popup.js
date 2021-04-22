805016493f74abd68c0ba598abbf365fc6f0bf0e
Yazhong Zhang <yazzhang@microsoft.com>
2021-04-22 22:57:17 +0800

    Core functions

---
+++ src/popup.js
@@ -0,0 +1,36 @@
+$('#homepage').click(function() {
+    chrome.tabs.create({
+        'url': 'https://github.com/zhangyazhong/WhoBookedMassage',
+        'active': true
+    }, function(tab) {});
+});
+
+$('.switch-input').click(function() {
+    let switchName = $(this).attr('id');
+    let option = $(this).is(':checked') ? 1 : 0;
+    setValueToChromStorage(switchName, option);
+});
+
+var showInTableSwitch = 'show-in-table-switch';
+var showInPopupSwitch = 'show-in-popup-switch';
+
+$(function () {
+    setSwitchOption(showInTableSwitch, 0);
+    setSwitchOption(showInPopupSwitch, 1);
+    $('body').removeAttr('style');
+});
+
+function setSwitchOption(switchName, defaultValue = 0) {
+    chrome.storage.local.get(switchName, function (result) {
+        let option = parseInt(result[switchName] === undefined ? defaultValue : result[switchName]);
+        setSwitchOnHtml(switchName, option);
+    });
+}
+
+function setSwitchOnHtml(name, option) {
+    $('#' + name).prop('checked', option === 1);
+}
+
+function setValueToChromStorage(name, value) {
+    chrome.storage.local.set({[name]: value}, function() {});
+}
---
+++ src/startup.js
@@ -0,0 +1,118 @@
+var showInTableSwitch = 'show-in-table-switch';
+var showInPopupSwitch = 'show-in-popup-switch';
+
+$(function() {
+    let eventRegx = /_list_event.push\(\{[^)]+\)/g;
+    let htmlSource = $('html').html();
+    let eventsRaw = htmlSource.match(eventRegx);
+    var eventsObj = eventsRaw.map(eventRaw => {
+        var id = getPropertyFromEventRaw(eventRaw, 'id');
+        var alias = getAliasFromEmail(getPropertyFromEventRaw(eventRaw, 'description'));
+        var status = getPropertyFromEventRaw(eventRaw, 'status');
+        return {
+            'id': id,
+            'alias': alias,
+            'statue': status
+        };
+    });
+
+    eventsObj.forEach(eventObj => console.log(eventObj));
+
+    chrome.storage.local.get(showInTableSwitch, function (result) {
+        let option = parseInt(result[showInTableSwitch] === undefined ? 0 : result[showInTableSwitch]);
+        if (option === 1) {
+            eventsRaw.forEach(eventRaw => {
+                var id = getPropertyFromEventRaw(eventRaw, 'id');
+                var alias = getAliasFromEmail(getPropertyFromEventRaw(eventRaw, 'description'));
+                var status = getPropertyFromEventRaw(eventRaw, 'status');
+                if (status.toLowerCase() === 'reserved') {
+                    $('.ca-elp' + id).html(alias);
+                }
+            });
+        }
+    });
+
+    chrome.storage.local.get(showInPopupSwitch, function (result) {
+        let option = parseInt(result[showInPopupSwitch] === undefined ? 1 : result[showInPopupSwitch]);
+        if (option === 1) {
+            let tooltips = [];
+            $('.chip.cbrd').each(function () {
+                let className = '';
+                for (className of $(this).attr('class').split(' ')) {
+                    if (className.includes('ca-evp')) {
+                        break;
+                    }
+                }
+
+                let eventId = className.substring(6);
+                let eventObj = getEventById(eventsObj, eventId);
+                tooltips.push({
+                    'eventId': eventId,
+                    'tooltip': new mdb.Tooltip($(this)[0], { title: 'Reserved by ' + eventObj.alias, trigger: 'manual' })
+                });
+            });
+            $('.chip.cbrd').mouseenter(function(e) {
+                let element = $(e.target);
+                while (!element.hasClass('chip') || !element.hasClass('cbrd')) {
+                    element = element.parent();
+                }
+
+                let className = '';
+                for (className of element.attr('class').split(' ')) {
+                    if (className.includes('ca-evp')) {
+                        break;
+                    }
+                }
+
+                let eventId = className.substring(6);
+                getTooltipById(tooltips, eventId).show();
+            });
+
+            $('.chip.cbrd').mouseleave(function(e) {
+                let element = $(e.target);
+                while (!element.hasClass('chip') || !element.hasClass('cbrd')) {
+                    element = element.parent();
+                }
+
+                let className = '';
+                for (className of element.attr('class').split(' ')) {
+                    if (className.includes('ca-evp')) {
+                        break;
+                    }
+                }
+
+                let eventId = className.substring(6);
+                getTooltipById(tooltips, eventId).hide();
+            });
+        }
+    });
+});
+
+function getEventById(eventsObj, id) {
+    for (var eventObj of eventsObj) {
+        if (eventObj.id === id) {
+            return eventObj;
+        }
+    }
+}
+
+function getTooltipById(tooltips, id) {
+    for (var tooltip of tooltips) {
+        if (tooltip.eventId == id) {
+            return tooltip.tooltip;
+        }
+    }
+}
+
+function getPropertyFromEventRaw(eventRaw, property) {
+    let propertyRegex = new RegExp("'" + property + "'[^,\\r\\n]+", 'g');
+    let propertyRaw = eventRaw.match(propertyRegex)[0];
+    let valueRaw = propertyRaw.split(':')[1].trim();
+    let valueRegex = /[^']+/g;
+    return valueRaw.match(valueRegex)[0];
+}
+
+function getAliasFromEmail(email)
+{
+    return email.split('@')[0];
+}
